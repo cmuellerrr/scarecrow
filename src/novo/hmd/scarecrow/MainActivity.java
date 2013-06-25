3a9aa470160b0be9ec1eaff6f8249ac7668096a6
Chris Mueller <cmuellerrr@gmail.com>
2013-06-25 15:41:29 -0700

    Removed unused imports.

--- src/novo/hmd/scarecrow/MainActivity.java
+++ src/novo/hmd/scarecrow/MainActivity.java
@@ -17,8 +17,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
-import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
@@ -30,7 +28,6 @@ import android.os.IBinder;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 
